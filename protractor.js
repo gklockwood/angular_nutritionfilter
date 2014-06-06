4b1b43d15c5c91d9c7e87788d1eaab51842b984d
Galen Lockwood <galenlockwood@gmail.com>
2014-06-06 09:39:46 -0400

    protractor update
    verbiage update for searching in protractor.js

--- protractor.js
+++ protractor.js
@@ -1,5 +1,5 @@
-  var expectFriendNames = function(expectedNames, key) {
-    element.all(by.repeater(key + ' in friends').column(key + '.name')).then(function(arr) {
+  var expectNutritionNames = function(expectedNames, key) {
+    element.all(by.repeater(key + ' in Nutritions').column(key + '.name')).then(function(arr) {
       arr.forEach(function(wd, i) {
         expect(wd.getText()).toMatch(expectedNames[i]);
       });
@@ -9,25 +9,25 @@
   it('should search across all fields when filtering with a string', function() {
     var searchText = element(by.model('searchText'));
     searchText.clear();
-    searchText.sendKeys('m');
-    expectFriendNames(['Mary', 'Mike', 'Adam'], 'friend');
+    searchText.sendKeys('a');
+    expectNutritionNames(['Apple', 'Apricot', 'Banana', 'Clementine'], 'Nutrition');
 
     searchText.clear();
-    searchText.sendKeys('76');
-    expectFriendNames(['John', 'Julie'], 'friend');
+    searchText.sendKeys('7');
+    expectNutritionNames(['Apricot', 'Cherry'], 'Nutrition');
   });
 
   it('should search in specific fields when filtering with a predicate object', function() {
     var searchAny = element(by.model('search.$'));
     searchAny.clear();
-    searchAny.sendKeys('i');
-    expectFriendNames(['Mary', 'Mike', 'Julie', 'Juliette'], 'friendObj');
+    searchAny.sendKeys('b');
+    expectNutritionNames(['Banana', 'Blueberry'], 'NutritionObj');
   });
   it('should use a equal comparison when comparator is true', function() {
     var searchName = element(by.model('search.name'));
     var strict = element(by.model('strict'));
     searchName.clear();
-    searchName.sendKeys('Julie');
+    searchName.sendKeys('Apple');
     strict.click();
-    expectFriendNames(['Julie'], 'friendObj');
+    expectNutritionNames(['Apple'], 'NutritionObj');
   });
